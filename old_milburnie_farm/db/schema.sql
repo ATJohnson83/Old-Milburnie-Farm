a2d6a5b45aa42c5c2298d9abdb6c0fbb40d8b1aa
Austin Johnson <ATJohnson83@Gmail.com>
2017-11-12 15:30:03 -0500

    fixed db name spelling

---
+++ old_milburnie_farm/db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS oldmilburnie;
+CREATE database oldmilburnie;
+
+-- USE oldbmillburne;
+
+-- CREATE TABLE tasks
+-- (
+--     id INT NOT NULL,
+    
+--     PRIMARY KEY (id)
+-- );
+
+
+-- CREATE TABLE employees
+-- (
+--     id INT NOT NULL,
+
+--     PRIMARY KEY (id)
+-- );
+
+
+-- CREATE TABLE manager
+-- (
+--     id INT NOT NULL,
+
+--     PRIMARY KEY (id)
+-- );
+
+
