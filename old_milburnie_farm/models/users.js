bf179ffa1c5010e2b9d980e32c3d1b60f283e4e9
David Palmquist <dpalmquist84@gmail.com>
2017-11-16 15:30:48 -0500

    changed users to reflect what Austin wants

--- old_milburnie_farm/models/users.js
+++ old_milburnie_farm/models/users.js
@@ -4,25 +4,25 @@ module.exports = function(sequelize, DataTypes) {
   var Users= sequelize.define("Users", {
     name: {
       type: DataTypes.STRING,
-      allowNull: true,
+      allowNull: false,
       validate: {
         len: [1]
       }
     },
     type: {
       type: DataTypes.STRING,
-      allowNull: true,
+      allowNull: false,
       len: [1]
     },
     username: {
       type: DataTypes.STRING,
-      allowNull: true,
+      allowNull: false,
       unique: true,
       len: [1]
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: true,
+      allowNull: false,
       len: [1]
     },
     active: {
