dfde2090a8fd57150777e2a8d732b96b40b90018
David Palmquist <dpalmquist84@gmail.com>
2017-11-20 10:26:20 -0500

    added html2canvas to jspdf

--- old_milburnie_farm/public/js/jsPDF.js
+++ old_milburnie_farm/public/js/jsPDF.js
@@ -16,14 +16,29 @@ var doc = new jsPDF();
 var specialElementHandlers = {
     '#editor': function (element, renderer) {
         return true;
+    },
+    '.controls' : function (element, renderer) {
+        return true;
     }
 };
 
 $('#printMgmtHarvest').click(function () {   
-    doc.fromHTML($('#activeHarvest').html(), 15, 15, {
-        'width': 170,
-            'elementHandlers': specialElementHandlers
-    });
-    doc.save('sample-file.pdf');
+    // doc.fromHTML($('#harvests').get(0), 15, 15, {
+    //     'width': 170,
+    //         'elementHandlers': specialElementHandlers
+    // });
+    // doc.save('sample-file.pdf');
+
+    html2canvas($("#harvests"), {
+            background : "#ffffff",
+            onrendered: function(canvas) {      
+                var imgData = canvas.toDataURL(
+                    'image/jpeg');              
+                var doc = new jsPDF('p', 'mm');
+                doc.addImage(imgData, 'jpeg', 10, 10);
+                doc.save('sample-file.pdf');
+            }
+        })
+    
 });
 
